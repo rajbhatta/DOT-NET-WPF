2ab25c4c8d0d95a71ca8ba9ba399b73cc0b7a5a3
Raj Bhatta <bhattaraj21@gmail.com>
2021-06-21 10:53:51 -0700

    Combox ItemSource

---
+++ 01-Combox/01-ComboxBoxItemSource/ComboxBoxItemSource/ComboxBoxViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ComboxBoxItemSource
+{
+    class ComboxBoxViewModel
+    {
+        public List<string> NameCollection { get; set; }
+        public List<Brush> BrushCollection { get; set; }
+
+        public ComboxBoxViewModel()
+        {
+            NameCollection = new List<string>()
+            {
+                "A",
+                "B",
+                "C"
+            };
+
+            BrushCollection = new List<Brush>()
+            {
+                Brushes.Red,
+                Brushes.Green,
+                Brushes.Blue,
+                Brushes.Orange
+            };
+        }
+    }
+}

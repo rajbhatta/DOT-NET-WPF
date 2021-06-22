f2f2706b53f49729107618deead3d15a9ce77fd8
Raj Bhatta <bhattaraj21@gmail.com>
2021-06-22 14:27:39 -0700

    How to use dispatcher in WPF

---
+++ 05-Using-dispatcher/Dispatcher/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dispatcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread th1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            th1 = new Thread(new ThreadStart(setText));
+            th1.Start();
+        }
+
+        public void setText()
+        {
+            
+            this.Dispatcher.Invoke(()=> { txtBox1.Text = "Multithreading example for use. "; }) ;
+        }
+    }
+}

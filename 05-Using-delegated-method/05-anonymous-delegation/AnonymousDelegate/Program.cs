f34f93558a012efb8a6ab0340e5ec9ad439423f2
Raj Bhatta <bhattaraj21@gmail.com>
2021-06-30 00:50:23 -0700

    Anonymous delegation function

---
+++ 05-Using-delegated-method/05-anonymous-delegation/AnonymousDelegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AnonymousDelegate
+{
+
+    public delegate string GreetingDelgate(string name);
+    class Program
+    {
+        public static string greeting(string name)
+        {
+            return "hello"+name;
+        }
+        static void Main(string[] args)
+        {
+           
+
+            GreetingDelgate obj = delegate (string name) {
+                return "Hello" + name;
+             };
+
+            obj.Invoke("A");
+        }
+    }
+}

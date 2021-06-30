a047271d1257dff72862bc7227228e611792ac71
Raj Bhatta <bhattaraj21@gmail.com>
2021-06-30 00:05:43 -0700

    Working with delegation

---
+++ 05-Using-delegated-method/03-example/Delegation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegation
+{
+    class Program
+    {
+        public delegate int MathOperation(int x, int y);
+        static void Main(string[] args)
+        {
+            MathOperation mathOperation = AddNumber;
+            Console.WriteLine(mathOperation(2, 3));
+
+            MathOperation math = Subtract;
+            Console.WriteLine(math(5, 2));
+
+        }
+
+        static public int AddNumber(int x, int y)
+        {
+            return x + y;
+        }
+
+        static public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}

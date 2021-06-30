4825954f539703c7b4580727189d346be4270980
Raj Bhatta <bhattaraj21@gmail.com>
2021-06-30 00:30:36 -0700

    Multicast delegation in c#

---
+++ 05-Using-delegated-method/04-multicast-delgation/MutlicastDelegation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MutlicastDelegation
+{
+
+    class Program
+    {
+        public delegate void PrintDelegate();
+
+        public static void Method1()
+        {
+            Console.WriteLine("This is method1");
+        }
+
+        public static void Method2()
+        {
+            Console.WriteLine("This is method2");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintDelegate myPrinter = Method1;
+            myPrinter();
+
+            //Using multicast that prints Method1 along with Method2
+            myPrinter += Method2;
+            myPrinter();
+        }
+    }
+}
+
